26ecdc5c8d3527323fee4dc705317440569423ca
AzamatKochimov <kochimovazamat180@gmail.com>
2025-12-14 20:28:09 +0500

    Update all with proper configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -158,6 +158,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:5173",
+    "https://sneakr-uz.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
